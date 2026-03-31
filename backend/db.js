697d3af368f0cfaaa43dce12b00295e78b195475
sheebasilvia07-png <sheebasilvia07@gmail.com>
2026-03-31 14:53:18 +0530

    fixed

--- backend/db.js
+++ backend/db.js
@@ -10,9 +10,9 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
   if (err) {
-    console.error("❌ DB ERROR:", err);
+    console.log("❌ DB connection failed:", err);
   } else {
-    console.log("✅ DB Connected");
+    console.log("✅ Connected to Railway MySQL");
   }
 });
 
